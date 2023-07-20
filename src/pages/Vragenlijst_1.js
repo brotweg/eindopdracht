d3ad4525e433c4528cdc718a54a099989e83f4fb
brotweg <mikejagerman@gmail.com>
2023-07-20 16:02:46 +0200

    data request werkt

--- src/pages/Vragenlijst_1.js
+++ src/pages/Vragenlijst_1.js
@@ -1,34 +1,56 @@
 import Pug from '../assets/Pug.png'
 import './Vragenlijst_1.css'
 import {useNavigate} from "react-router-dom";
-import axios from 'Axios';
+import axios from 'axios';
+import {useState} from "react";
+
 
 
 
 
 function VragenlijstEen() {
     const navigate = useNavigate();
-    const name = 'Golden Retriever';
+    const name = 'golden retriever';
+    const apiKey = 'OvzRvYsVHUUcIgk5PKl5Brg1I4eWGN7toxXd1mvk';
+    const config = {
+        headers:{
+            'X-Api-Key': apiKey,
+        }
+    };
+    const [dogData, setDogData] = useState({});
 
     async function fetchData() {
         try {
-            const result = await axios.get('https://api.api-ninjas.com/v1/dogs?name=' + name, {
-                'Accept': 'application/json',
-                'X-Api-Key': 'C6RZGKfUk0YgsX66tZ3zgw==nh9C36MXgSXHP6aF'
-            });
+            const result = await axios.get('https://api.api-ninjas.com/v1/dogs?name=' + name, config);
             console.log(result);
+            setDogData(result.data);
         } catch(e) {
             console.error(e);
         }
     }
+
+    Object.keys(dogData).length > 0 && console.log(dogData[0].name)
 
     return(
         <>
             <div className="content_wrapper">
                 <div className="information">
                     <h1>Jouw wensen</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti itaque ullam velit vitae? Atque dolorem minima provident quos recusandae. Doloremque, exercitationem, perspiciatis. Deleniti eaque enim maiores officia quas reprehenderit sed tenetur ullam velit? Blanditiis dolorem ducimus earum laudantium maxime minus omnis sit temporibus. Aperiam distinctio esse eveniet facere, laudantium placeat.
-                    </p>
+                    <button
+                        type="button"
+                        onClick={fetchData}
+                    >
+                        Haal data op!
+                    </button>
+                        <span>
+      {Object.keys(dogData).length > 0 &&
+          <>
+              <h2>{dogData[0].name}</h2>
+
+          </>
+      }
+    </span>
+                    Je hebt gezocht op: {dogData.name}
                 </div>
                 <img src={Pug} className="pug_image" alt="Pug" />
                 <button className="button_bottom" onClick={() => navigate('/vragenlijst_2')}>Ga Verder</button>
