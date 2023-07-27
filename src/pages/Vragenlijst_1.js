d00a3d71ba17416a4e52d0556810e0d3ccd2a9cc
brotweg <mikejagerman@gmail.com>
2023-07-27 17:21:59 +0200

    opmaak resultaten aangepast

--- src/pages/Vragenlijst_1.js
+++ src/pages/Vragenlijst_1.js
@@ -8,56 +8,48 @@ import {useState} from "react";
 
 
 
+
 function VragenlijstEen() {
     const navigate = useNavigate();
     const apiKey = 'OvzRvYsVHUUcIgk5PKl5Brg1I4eWGN7toxXd1mvk';
 
     const [dogData, setDogData] = useState({});
-    const [shedding, setShedding] = useState(1);
-    const [barking, setBarking] = useState(1);
+    const [loading, toggleLoading] = useState(false);
     const [energy, setEnergy] = useState(1);
+    const [protectiveness, setProtectiveness] = useState(1);
+    const [trainability, setTrainability] = useState(1);
+
 
     async function fetchData(offset) {
-        let moreDogData = [];
+        toggleLoading(true);
         for (let i = 1; i <= 10; i++) {
-        try {
+            try {
                 const result = await axios.get('https://api.api-ninjas.com/v1/dogs',
                     {
                         params: {
-                            shedding: shedding,
-                            barking: barking,
                             energy: energy,
+                            protectiveness: protectiveness,
+                            trainability: trainability,
                             offset,
-
                         },
                         headers: {
                             'X-Api-Key': apiKey,
                         }
                     }
                 );
+                toggleLoading(false);
 
-            return result.data;
+                return result.data;
 
             } catch (e) {
                 console.error(e);
-            return [];
+                toggleLoading(false);
+
             }
         }
-}
-
-
-    function handleSliderChangeShedding(e){
-        setShedding(e.target.value);
     }
 
-    function handleSliderChangeBarking(e) {
-        setBarking(e.target.value);
-    }
-
-    function handleSliderChangeEnergy(e) {
-        setEnergy(e.target.value);
-    }
-   async function handleClick(e) {
+    async function handleClick(e) {
         e.preventDefault();
         try {
             const resultsPerPage = 20; // Number of results per page
@@ -70,51 +62,85 @@ function VragenlijstEen() {
                 moreDogData = moreDogData.concat(result);
                 offset += resultsPerPage;
             }
-
             setDogData(moreDogData);
         } catch (error) {
             console.error(error);
         }
     }
 
+    console.log(dogData);
+
+    function handleSliderChangeEnergy(e) {
+        setEnergy(e.target.value);
+    }
+
+    function handleSliderChangeProtectiveness(e) {
+        setProtectiveness(e.target.value);
+    }
+
+    function handleSliderChangeTrainability(e) {
+        setTrainability(e.target.value);
+    }
+
+    function sliderTextEnergy(energy) {
+        switch (energy) {
+            case '1':
+                return 'Lui';
+            case '2':
+                return 'Rustig';
+            case '3':
+                return 'Gemiddeld';
+            case '4':
+                return 'Redelijk actief';
+            case '5':
+                return 'Altijd druk';
+            default:
+                return '';
+        }
+    }
+
+    function sliderTextProtectiveness(protectiveness) {
+        switch (protectiveness) {
+            case '1':
+                return 'Allemansvriend';
+            case '2':
+                return 'Niet erg waakzaam';
+            case '3':
+                return 'Gemiddeld';
+            case '4':
+                return 'Redelijk waakzaam';
+            case '5':
+                return 'Bodyguard';
+            default:
+                return '';
+        }
+    }
+
+    function sliderTextTrainability(trainability) {
+        switch (trainability) {
+            case '1':
+                return 'Oost-Indisch doof';
+            case '2':
+                return 'Luistert als hij zin heeft';
+            case '3':
+                return 'Gemiddeld';
+            case '4':
+                return 'Redelijk goed te trainen';
+            case '5':
+                return 'Circushond';
+            default:
+                return '';
+        }
+    }
+
     return(
         <>
             <div className="content_wrapper">
-                <div className="information">
+                <div className="vragenlijst_1">
                     <h1>Jouw wensen</h1>
                     <form>
                         <section>
-                            <label htmlFor="shedding_slider">Hoeveel mag de hond uitharen?</label>
-                            <input
-                                name="shedding"
-                                id="shedding_slider"
-                                type="range"
-                                min="1"
-                                max="5"
-                                step="1"
-                                value={shedding}
-                                onChange={handleSliderChangeShedding}
-                            />
-                            <span>{shedding}</span>
-
-                       </section>
-                        <section>
-                            <label htmlFor="barking_slider">Hoeveel mag de hond blaffen?</label>
-                            <input
-                                name="barking"
-                                id="barking_slider"
-                                type="range"
-                                min="1"
-                                max="5"
-                                step="1"
-                                value={barking}
-                                onChange={handleSliderChangeBarking}
-                            />
-                            <span>{barking}</span>
-
-                        </section>
-                        <section>
-                            <label htmlFor="barking_slider">Hoe energiek mag de hond zijn?</label>
+                            <label htmlFor="energy_slider">Hoe energiek mag de hond zijn?</label>
                             <input
                                 name="energy"
                                 id="energy_slider"
@@ -125,23 +151,68 @@ function VragenlijstEen() {
                                 value={energy}
                                 onChange={handleSliderChangeEnergy}
                             />
-                            <span>{energy}</span>
+                            <span>{sliderTextEnergy(energy)}</span>
 
                         </section>
-                       <button type="submit" onClick={handleClick}>Verzend</button>
+                        <section>
+                            <label htmlFor="protectiveness_slider">Hoe waakzaam mag de hond zijn?</label>
+                            <input
+                                name="protectiveness"
+                                id="protectiveness_slider"
+                                type="range"
+                                min="1"
+                                max="5"
+                                step="1"
+                                value={protectiveness}
+                                onChange={handleSliderChangeProtectiveness}
+                            />
+                            <span>{sliderTextProtectiveness(protectiveness)}</span>
+
+                        </section>
+                        <section>
+                            <label htmlFor="trainability_slider">Hoe trainbaar moet de hond zijn?</label>
+                            <input
+                                name="trainability"
+                                id="trainability_slider"
+                                type="range"
+                                min="1"
+                                max="5"
+                                step="1"
+                                value={trainability}
+                                onChange={handleSliderChangeTrainability}
+                            />
+                            <span>{sliderTextTrainability(trainability)}</span>
+
+                        </section>
+
+                        <button type="submit" onClick={handleClick} className="vragenlijst_1_button">Verzend</button>
+
                     </form>
 
 
-      {Object.keys(dogData).length > 0 &&
-          <>
-              Je hebt gezocht op:
-              <ul>
-                  {dogData.map((dog, index) => (
-                      <li key={index}>{dog.name}</li>
-                  ))}
-              </ul>
-          </>
-      }
+                    {loading &&
+                        <p>Aan het laden...</p>
+                    }
+
+                    {Object.keys(dogData).length > 0 && loading === false &&
+                        <>
+                            <ul className="dog_breed_list">
+                                {dogData.map((dog, index) => (
+                                    <>
+                                        <li key={index}>
+                                            <h3>{dog.name}</h3>
+                                            <img src={dog.image_link}/>
+                                        </li>
+                                    </>
+                                ))}
+                            </ul>
+                        </>
+                    }
+                    {Object.keys(dogData).length === 0 && loading === false &&
+                        <>
+                            <p>Er zijn helaas geen resultaten die aan jouw criteria voldoen.</p>
+                        </>
+                    }
 
 
                 </div>
